96337f0d354b165eabba331f74ab5d5327682211
dragonayan <ayan.cse120067@bppimt.ac.in>
2023-08-06 23:36:01 +0530

    hi

--- java/sliding window/FindNegative.java
+++ java/sliding window/FindNegative.java
@@ -1,14 +1,41 @@
- class Ans {
+import java.util.*;
+
+class Ans {
     public static void func(int[] arr, int k) {
-        System.out.println(k);
+        int i = 0, j = 0;
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int size = arr.length;
+        while (j < size) {
+            if (j - i + 1 < k) {
+                if (arr[j] < 0) {
+                    ans.add(arr[j]);
+                }
+                j++;
+                ans.add(arr[j]);
+            } 
+            else if (j - i + 1 == k) {
+                if (arr[j] < 0) {
+                    ans.add(arr[j]);
+
+                    break;
+                } else {
+                    ans.add(0);
+                }
+                i++;
+                j++;
+            }
+        }
+        System.out.println(ans);
+
     }
 }
 
 public class FindNegative {
 
     public static void main(String[] args) {
-        int[] arr = { 1, 3, 2, 4, 5, 9, 2, 4, 7 };
+        int[] arr = { 1, -3, 2, -4, 5, -9, 2, 4, 7 };
 
-        Ans.func(arr, 4);
+        Ans.func(arr, 3);
     }
 }
