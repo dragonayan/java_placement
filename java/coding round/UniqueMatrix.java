be23c13784b45eac36194fff83d781d1b2c442a1
dragonayan <ayan.cse120067@bppimt.ac.in>
2023-08-20 12:11:01 +0530

    uniqueMatrix

--- java/coding round/UniqueMatrix.java
+++ java/coding round/UniqueMatrix.java
@@ -1,5 +1,41 @@
-public class UniqueMatrix{
+import java.util.*;
+
+public class UniqueMatrix {
+    public static boolean func(int[][] arr, int m, int n) {
+        HashSet<Integer> h1 = new HashSet<>();
+        HashSet<Integer> h2 = new HashSet<>();
+        for (int i = 0; i < m; i++) {
+            h1.clear();
+            h2.clear();
+
+            for (int j = 0; j < m; j++) {
+                h1.add(arr[i][j]);
+                h2.add(arr[j][i]);
+
+            }
+            if (h1.size() != h2.size()) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        boolean ans = func(arr, m, n);
+        if (ans) {
+            System.out.println("TRue");
+        } else {
+            System.out.println("False");
+        }
     }
 }
